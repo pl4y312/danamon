11162c59be46c72489a9320c0926db28c5cde6f2
pl4y312 <pl4y312@yahoo.com>
2017-03-13 02:39:34 +0700

    80% Update

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,16 +1,81 @@
 $(document).ready(function(){
-	$(window).scroll(function() {
-	    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
-	        $('#gototop')
-	        	.fadeIn(500)
-	        	.css('left', $('#container .wrap').offset().left + $('#container .wrap').width() - 61);    // Fade in the arrow
-	    } else {
-	        $('#gototop').fadeOut(500);   // Else fade out the arrow
-	    }
+
+	/**
+	*
+	* Slideshow
+	*
+	**/
+	
+	$('#slideshow .item').css('display', 'none');
+	$('#slideshow .item').first().fadeIn().addClass('active');
+	var slideInt = setInterval( "slideshow()", 3000);
+
+	$('#slideshow-nav').append(function(){
+		var nav = '<ul>';
+
+		for(i=0; i < $('#slideshow .item').length; i++){
+			nav += '<li data-id="' + i + '"></li>';
+		}
+		nav += '</ul>';
+
+		return nav;
+	}).css("margin-left", function(){
+		return -$(this).width()/2;
 	});
-	$('#gototop').click(function() {      // When arrow is clicked
-	    $('body').animate({
-	        scrollTop : 0                       // Scroll to top of body
-	    }, 500);
+
+	$('#slideshow-nav li').first().addClass('active');
+
+	$('#slideshow-nav li').on('click', function(){
+
+		clearInterval(slideInt);
+
+		$('#slideshow-nav li.active').removeClass('active');
+		$(this).addClass('active');
+
+		var index = $(this).data('id');
+
+		$('#slideshow .item.active').fadeOut().removeClass('active');
+		$("#slideshow .item:eq(" + index + ")").fadeIn().addClass('active');
+
+		slideInt = setInterval( "slideshow()", 3000);
+
+	});
+
+	/**
+	*
+	* Scroll to top
+	*
+	**/
+	$(window).scroll(function() {
+		if ($(this).scrollTop() >= 50) {
+			$('#gototop')
+				.fadeIn(500)
+				.css('left', $('#container .wrap').offset().left + $('#container .wrap').width() - 61);    // Fade in the arrow
+		} else {
+			$('#gototop').fadeOut(500);
+		}
+	});
+	$('#gototop').click(function() {
+		$('body').animate({
+			scrollTop : 0
+		}, 500);
 	});
 });
+
+function slideshow(){
+	var $active = $('#slideshow .item.active');
+	var $next = $active.next('.item');
+
+	$active.fadeOut().removeClass('active');
+	$('#slideshow-nav li.active').removeClass('active');
+
+	if($next.length > 0){
+		$next.fadeIn().addClass('active');
+	} else {
+		$('#slideshow .item').first().fadeIn().addClass('active');
+	}
+
+	var activeIndex = $('#slideshow .item.active').index();
+	$('#slideshow-nav li:eq(' + activeIndex + ')').addClass('active');
+
+}
